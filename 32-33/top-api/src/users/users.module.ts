2efdd528490aa5dae00fe2b969b11bab9589e057
Valery <11w-walerik@mail.ru>
2023-06-29 16:22:46 +0300

    33 done

--- 32-33/top-api/src/users/users.module.ts
+++ 32-33/top-api/src/users/users.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './models/user.model';
+import { Post, PostSchema } from './models/post.model';
 
 @Module({
 	providers: [UsersService],
-	imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+	imports: [
+		MongooseModule.forFeature([
+			{ name: User.name, schema: UserSchema },
+			{ name: Post.name, schema: PostSchema },
+		]),
+	],
 })
 export class UsersModule {}
