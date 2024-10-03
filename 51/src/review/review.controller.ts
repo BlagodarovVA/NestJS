dbc373f5b7a86bf4bcdf997d479c23a85549a4b9
Valery Blagodarov <11w-walerik@mail.ru>
2024-10-03 10:48:18 +0300

    51 start

---
+++ 51/src/review/review.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { REVIEW_NOT_FOUND } from './review.constants';
+import { ReviewService } from './review.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
+
+@Controller('review')
+export class ReviewController {
+	constructor(private readonly reviewService: ReviewService) { }
+
+	@UsePipes(new ValidationPipe())
+	@Post('create')
+	async create(@Body() dto: CreateReviewDto) {
+		return this.reviewService.create(dto);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		const deletedDoc = await this.reviewService.delete(id);
+		if (!deletedDoc) {
+			throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get('byProduct/:productId')
+	async getByProduct(@Param('productId') productId: string, @UserEmail() email: string) {
+		return this.reviewService.findByProductId(productId);
+	}
+}
