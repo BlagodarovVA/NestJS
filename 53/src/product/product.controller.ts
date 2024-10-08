b8ee1131547e51bb39557f68ffde61e62f6fd54a
Valery Blagodarov <11w-walerik@mail.ru>
2024-10-08 15:59:08 +0300

    53 done

---
+++ 53/src/product/product.controller.ts
@@ -0,0 +1,49 @@
+import { Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { FindProductDto } from './dto/find-product.dto';
+import { ProductModel } from './product.model';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ProductService } from './product.service';
+import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
+
+@Controller('product')
+export class ProductController {
+	constructor(private readonly productService: ProductService) {}
+
+	@Post('create')
+	async create(@Body() dto: CreateProductDto) {
+		return this.productService.create(dto);
+	}
+
+	@Get(':id')
+	async get(@Param('id') id: string) {
+		const product = await this.productService.findById(id);
+		if (!product) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
+		return product;
+	}
+
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		const deletedProduct = await this.productService.deleteById(id);
+		if (!deletedProduct) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
+	}
+
+	@Patch(':id')
+	async patch(@Param('id') id: string, @Body() dto: ProductModel) {
+		const updatedProduct = await this.productService.updateById(id, dto);
+		if (!updatedProduct) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
+		return updatedProduct;
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('find')
+	async find(@Body() dto: FindProductDto) {
+		return this.productService.findWithReviews(dto);
+	}
+}
