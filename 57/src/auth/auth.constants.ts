5efb5ba8d3a5d529da72a046a493ce32fef2bbcf
Valery Blagodarov <11w-walerik@mail.ru>
2024-10-09 13:52:30 +0300

    57 done

---
+++ 57/src/auth/auth.constants.ts
@@ -0,0 +1,3 @@
+export const ALREADY_REGISTERED_ERROR = 'Такой пользователь уже зарегистрирован';
+export const USER_NOT_FOUND_ERROR = 'Пользователь с таким email не найден';
+export const WRONG_PASSWORD_ERROR = 'Пароль введен не правильно';
