a5f363a52bf01f8b8d856603f2116a6605de9bea
Valery Blagodarov <11w-walerik@mail.ru>
2024-10-10 14:16:29 +0300

    59 done

---
+++ 59/src/auth/auth.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { AuthController } from './auth.controller';
+import { UserModel } from './user.model';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTConfig } from 'src/configs/jwt.config';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+
+@Module({
+	controllers: [AuthController],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: UserModel,
+				schemaOptions: {
+					collection: 'User'
+				}
+			}
+		]),
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTConfig
+		}),
+		PassportModule,
+	],
+	providers: [AuthService, JwtStrategy]
+})
+export class AuthModule { }
