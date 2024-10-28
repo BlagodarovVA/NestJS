3a8f2b5e3e881bb550fff25272aa849345de19d1
Valery Blagodarov <11w-walerik@mail.ru>
2024-10-28 11:46:00 +0300

    64 done

---
+++ 64/src/top-page/top-page.controller.ts
@@ -0,0 +1,68 @@
+import { Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, Patch, Post, UsePipes, UseGuards, ValidationPipe } from '@nestjs/common';
+import { FindTopPageDto } from './dto/find-top-page.dto';
+import { TopPageService } from './top-page.service';
+import { CreateTopPageDto } from './dto/create-top-page.dto';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
+import { NOT_FOUND_TOP_PAGE_ERROR } from './top-page.constants';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+
+@Controller('top-page')
+export class TopPageController {
+	constructor(private readonly topPageService: TopPageService) {}
+
+	@UseGuards(JwtAuthGuard)
+	@Post('create')
+	async create(@Body() dto: CreateTopPageDto) {
+		return this.topPageService.create(dto);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get(':id')
+	async get(@Param('id', IdValidationPipe) id: string) {
+		const page = await this.topPageService.findById(id);
+		if (!page) {
+			throw new NotFoundException(NOT_FOUND_TOP_PAGE_ERROR);
+		}
+		return page;
+	}
+
+	@Get('byAlias/:alias')
+	async getByAlias(@Param('alias') alias: string) {
+		const page = await this.topPageService.findByAlias(alias);
+		if (!page) {
+			throw new NotFoundException(NOT_FOUND_TOP_PAGE_ERROR);
+		}
+		return page;
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		const deletedPage = await this.topPageService.deleteById(id);
+		if (!deletedPage) {
+			throw new NotFoundException(NOT_FOUND_TOP_PAGE_ERROR);
+		}
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Patch(':id')
+	async patch(@Param('id') id: string, @Body() dto: CreateTopPageDto) {
+		const updatedPage = await this.topPageService.updateById(id, dto);
+		if (!updatedPage) {
+			throw new NotFoundException(NOT_FOUND_TOP_PAGE_ERROR);
+		}
+		return updatedPage;
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('find')
+	async find(@Body() dto: FindTopPageDto) {
+		return this.topPageService.findByCategory(dto.firstCategory);
+	}
+
+	@Get('textSearch/:text')
+	async textSearch(@Param('text') text: string) {
+		return this.topPageService.findByText(text);
+	}
+}
